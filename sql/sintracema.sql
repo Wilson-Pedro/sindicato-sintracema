28aa707a1053fc837546e250b7f484158c2e6f42
NeewJax <202203577841@alunos.estacio.br>
2023-05-21 03:14:56 -0300

    new table filiais:   sql/sintracema.sql

--- sql/sintracema.sql
+++ sql/sintracema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15-Maio-2023 às 01:03
+-- Tempo de geração: 21-Maio-2023 às 08:13
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -20,6 +20,46 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `sintracema`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `filiais`
+--
+
+CREATE TABLE `filiais` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `uf` varchar(50) DEFAULT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `nacionalidade` varchar(100) NOT NULL,
+  `naturalidade` varchar(100) NOT NULL,
+  `escolaridade` varchar(50) NOT NULL,
+  `cursos` varchar(255) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `estado_civil` varchar(255) NOT NULL,
+  `nascimento` date NOT NULL,
+  `fone` varchar(20) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `rg` varchar(20) NOT NULL,
+  `nome_pai` varchar(255) NOT NULL,
+  `nome_mae` varchar(255) NOT NULL,
+  `nome_conjuge` varchar(255) NOT NULL,
+  `nome_filhos` varchar(255) NOT NULL,
+  `assinatura_socio` varchar(255) NOT NULL,
+  `numero_matricula` int(100) NOT NULL,
+  `registrado` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `filiais`
+--
+
+INSERT INTO `filiais` (`id`, `nome`, `uf`, `cidade`, `nacionalidade`, `naturalidade`, `escolaridade`, `cursos`, `endereco`, `estado_civil`, `nascimento`, `fone`, `cpf`, `rg`, `nome_pai`, `nome_mae`, `nome_conjuge`, `nome_filhos`, `assinatura_socio`, `numero_matricula`, `registrado`) VALUES
+(1, 'Marcos Souza Silva', 'Maranhão', 'São Luís', 'Brasileiro', 'São Luís', 'medio', 'Administração', 'Avenida Principal', 'Solteiro', '1998-02-04', '98991191467', '882.273.160-34', '52.564-00', 'Leonardo Julio', 'Maria Silva Joaquina', 'Isabella Moreira', 'Anderson Silva Costa', 'Fernando Alves', 2374612, '2023-05-21 05:35:09'),
+(2, 'Marcos Souza Silva', 'Maranhão', 'São Luís', 'Brasileiro', 'São Luís', 'medio', 'Administração', 'Avenida Principal', 'casado', '1998-02-04', '98991191467', '882.273.160-34', '52.564-00', 'Leonardo Julio', 'Maria Silva Joaquina', 'Isabella Moreira', 'Anderson Silva Costa', 'Fernando Alves', 2374612, '2023-05-21 05:34:57'),
+(3, 'David Silva Souza', 'Maranhão', 'São Luís', 'Brasileiro', 'São Luís', 'medio', 'Nutrição', 'Avenida dois', 'casado', '2001-12-12', '7567576547', '4234242344', '41343244', 'João da Silva', 'Maria Silva', 'Isabella', 'Anderson', 'Fernando Souza', 23498324, '2023-05-21 05:34:06'),
+(12, 'Bruna Silva Rodrigues', 'MA', '', 'Brasileira', 'São Luís', 'medio', 'Educação Física', 'Coroadinho', 'solteiro', '1999-03-02', '989149312', '612.412.123-55', '43.123-31', 'Manoel Alves de Souza', 'Emilly Silva Rodrigues', '', '', 'Kaio Alves', 13432432, '2023-05-21 06:11:28');
 
 -- --------------------------------------------------------
 
@@ -47,6 +87,12 @@ INSERT INTO `users` (`id`, `nome`, `email`, `senha`, `data`) VALUES
 --
 
 --
+-- Índices para tabela `filiais`
+--
+ALTER TABLE `filiais`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices para tabela `users`
 --
 ALTER TABLE `users`
@@ -55,6 +101,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `filiais`
+--
+ALTER TABLE `filiais`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `users`
