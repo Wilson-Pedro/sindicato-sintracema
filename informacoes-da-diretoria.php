b81a5319a2137c8e9a91dd9d73236fa2ac768c41
Squarae <104042529+Squarae@users.noreply.github.com>
2023-06-03 11:42:05 -0300

    css responsivo: páginas de cadastro de filiado e infomações da diretoria. E mudança da cor de fundo da página inicial.

--- informacoes-da-diretoria.php
+++ informacoes-da-diretoria.php
@@ -6,97 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/info-diretoria.css">
     <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
     <title>Noticias e informações da diretoria</title>
-    <style>
-      @media screen{
-        .manssage-info
-        {
-          display: none;
-        }
-      }
-      @media only screen and (max-width: 600px)
-      {
-        .manssage-info
-        {
-          display: block;
-          text-align: center;
-        }
-      }
-        body
-        {
-            background-color: rgba(0, 0, 0);
-        }
-        header
-        {
-          margin-bottom: 10vh;
-        }
-        main, footer
-        {
-            background-color: rgba(255, 255, 255);
-            box-shadow: 0px 5px 50px rgb(255, 255, 255,0.500); 
-            margin-bottom: 2vh;
-            margin-left: 5vh;
-            margin-right: 5vh;
-            border-radius: 2vh;
-            padding: 2vh;
-        }
-        .carousel-control-prev:hover, .carousel-control-next:hover
-        {
-            background-color: black;
-
-        }
-        .carousel-caption
-        {
-            background-color: rgba(0, 0, 0, 0.500);
-            border-radius: 2vh;
-        }
-        .table
-        {
-            text-align: center;
-        }
-        .massange-box
-        {
-            text-align: center;
-            background-color: grey;
-            bottom: 0vh;
-            padding: 2vh;
-        }
-        .display-3
-        {
-          background-image: linear-gradient(to bottom, transparent 0%, transparent 90%, black 90%, black 100%);
-          background-repeat: no-repeat;
-          background-size: 0% 100%;
-          background-position-x: right;
-          transition: background-size 300ms;
-        }
-        .display-3:hover
-        {
-          background-size: 100% 100%;
-          background-position-x: left;
-        }
-        .navbar
-        {
-            box-shadow: 0px 5px 50px rgba(0, 0, 0, 0.5); 
-        }
-        .d-block
-        {
-          border-radius: 2vh;
-        }
-        .rodape {
-          background-color: #15171a;
-          color: white;
-          font-weight: bold;
-          text-align: center;
-        }
-    </style>
 </head>
 <body>
 <div class="container-fluid">
     <header>
       <nav class="navbar navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">SINDICATO SINTRACEMA</a>
+          <a class="navbar-brand" href="index.php">SINDICATO SINTRACEMA</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar"
             aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -195,20 +114,15 @@
             </h3>
             <br>
             <hr>
-            <p class="manssage-info">->->-> arraste para o lado ->->-></p>
+            <p class="manssage-info"> arraste para baixo </p>
             <br>
         </div>
+
+        
+        <!-- CARDS DE INFOS 1ª FILEIRA -->
         <div class="table-responsive-sm">
-            <table class="table">
-                <tr>
-                    <th scope="col">
-                    </th>
-                    <th scope="col">
-                    </th>
-                    <th scope="col">
-                    </th>
-                    <th scope="col">
-                        <div class="card" style="width: 18rem;">
+                      <!-- CARDS DE INFOS -->
+                        <div class="card">
                             <img src="img/info-diretoria/A semana iniciou com bastante trabalho_ o SINTRACEMA (1).JPG" class="card-img-top" loading="lazy" alt="Noticia:  A semana iniciou com bastante trabalho, o SINTRACEMA-Sindicato dos Agentes de Combate às Endemias e Sindacs- Sindicato dos Agentes Comunitários de Saúde, esteve em uma reunião nesta manhã (11.04), na Prefeitura Municipal de São Luís, com Secretário de Governo o Sr. Emílio Carlos Murad e o Vereador Pavão Filho, na oportunidade protocolamos e entregamos em mãos a minuta do Projeto de Lei que versa a criação do Plano de Cargos e Carreiras dos ACE e ACS.
                             Ainda estivemos protocolando dois ofícios, um solicitando uma reunião com urgência com a Superintendente da Vigilância Sanitária, e o outro com o Secretário Municipal de Saúde de São Luís.">
                             <div class="card-body">
@@ -217,9 +131,7 @@
                               <a href="https://www.instagram.com/p/Cq3EmknOFTk/" class="btn btn-primary">Ver noticia</a>
                             </div>
                         </div>
-                    </th>
-                    <th scope="col">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card">
                             <img src="img/info-diretoria/A semana iniciou com bastante trabalho_ o SINTRACEMA (2).JPG" class="card-img-top" loading="lazy" alt="Noticia:  A semana iniciou com bastante trabalho, o SINTRACEMA-Sindicato dos Agentes de Combate às Endemias e Sindacs- Sindicato dos Agentes Comunitários de Saúde, esteve em uma reunião nesta manhã (11.04), na Prefeitura Municipal de São Luís, com Secretário de Governo o Sr. Emílio Carlos Murad e o Vereador Pavão Filho, na oportunidade protocolamos e entregamos em mãos a minuta do Projeto de Lei que versa a criação do Plano de Cargos e Carreiras dos ACE e ACS.
                             Ainda estivemos protocolando dois ofícios, um solicitando uma reunião com urgência com a Superintendente da Vigilância Sanitária, e o outro com o Secretário Municipal de Saúde de São Luís.">
                             <div class="card-body">
@@ -227,9 +139,8 @@
                               <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, dolorem!</p>
                               <a href="https://www.instagram.com/p/Cq3EmknOFTk/" class="btn btn-primary">Ver noticia</a>
                         </div>
-                    </th>
-                    <th scope="col">
-                        <div class="card" style="width: 18rem;">
+                        </div>
+                        <div class="card">
                             <img src="img/info-diretoria/A semana iniciou com bastante trabalho_ o SINTRACEMA (3).JPG" class="card-img-top" loading="lazy" alt="Noticia:  A semana iniciou com bastante trabalho, o SINTRACEMA-Sindicato dos Agentes de Combate às Endemias e Sindacs- Sindicato dos Agentes Comunitários de Saúde, esteve em uma reunião nesta manhã (11.04), na Prefeitura Municipal de São Luís, com Secretário de Governo o Sr. Emílio Carlos Murad e o Vereador Pavão Filho, na oportunidade protocolamos e entregamos em mãos a minuta do Projeto de Lei que versa a criação do Plano de Cargos e Carreiras dos ACE e ACS.
                             Ainda estivemos protocolando dois ofícios, um solicitando uma reunião com urgência com a Superintendente da Vigilância Sanitária, e o outro com o Secretário Municipal de Saúde de São Luís.">
                             <div class="card-body">
@@ -237,12 +148,13 @@
                               <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, dolorem!</p>
                               <a href="https://www.instagram.com/p/Cq3EmknOFTk/" class="btn btn-primary">Ver noticia</a>
                         </div>
-                    </th>
-                </tr>
-            </table>
-            </div>
+                        </div>
+                      </div>
+
+
     </main>
     <footer>
+      <div class="eventos-box">
         <div>
             <br>
             <hr>
@@ -252,20 +164,15 @@
             </h3>
             <br>
             <hr>
-            <p class="manssage-info">->->-> arraste para o lado ->->-></p>
+            <p class="manssage-info"> arraste para baixo </p>
             <br>
         </div>
+
+
+        <!-- CADS DE EVENTOS 1ª FILEIRA -->
         <div class="table-responsive-sm">
-            <table class="table">
-                <tr>
-                    <th scope="col">
-                    </th>
-                    <th scope="col">
-                    </th>
-                    <th scope="col">
-                    </th>
-                    <th scope="col">
-                        <div class="card" style="width: 18rem;">
+                      <!-- CARDS DE EVENTOS -->
+                        <div class="card">
                             <img src="img/info-diretoria/A semana iniciou com bastante trabalho_ o SINTRACEMA (1).JPG" class="card-img-top" loading="lazy" alt="Noticia:  A semana iniciou com bastante trabalho, o SINTRACEMA-Sindicato dos Agentes de Combate às Endemias e Sindacs- Sindicato dos Agentes Comunitários de Saúde, esteve em uma reunião nesta manhã (11.04), na Prefeitura Municipal de São Luís, com Secretário de Governo o Sr. Emílio Carlos Murad e o Vereador Pavão Filho, na oportunidade protocolamos e entregamos em mãos a minuta do Projeto de Lei que versa a criação do Plano de Cargos e Carreiras dos ACE e ACS.
                             Ainda estivemos protocolando dois ofícios, um solicitando uma reunião com urgência com a Superintendente da Vigilância Sanitária, e o outro com o Secretário Municipal de Saúde de São Luís.">
                             <div class="card-body">
@@ -274,9 +181,7 @@
                               <a href="https://www.instagram.com/p/Cq3EmknOFTk/" class="btn btn-primary">Ver noticia</a>
                             </div>
                         </div>
-                    </th>
-                    <th scope="col">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card">
                             <img src="img/info-diretoria/A semana iniciou com bastante trabalho_ o SINTRACEMA (2).JPG" class="card-img-top" loading="lazy" alt="Noticia:  A semana iniciou com bastante trabalho, o SINTRACEMA-Sindicato dos Agentes de Combate às Endemias e Sindacs- Sindicato dos Agentes Comunitários de Saúde, esteve em uma reunião nesta manhã (11.04), na Prefeitura Municipal de São Luís, com Secretário de Governo o Sr. Emílio Carlos Murad e o Vereador Pavão Filho, na oportunidade protocolamos e entregamos em mãos a minuta do Projeto de Lei que versa a criação do Plano de Cargos e Carreiras dos ACE e ACS.
                             Ainda estivemos protocolando dois ofícios, um solicitando uma reunião com urgência com a Superintendente da Vigilância Sanitária, e o outro com o Secretário Municipal de Saúde de São Luís.">
                             <div class="card-body">
@@ -284,9 +189,8 @@
                               <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, dolorem!</p>
                               <a href="https://www.instagram.com/p/Cq3EmknOFTk/" class="btn btn-primary">Ver noticia</a>
                         </div>
-                    </th>
-                    <th scope="col">
-                        <div class="card" style="width: 18rem;">
+                        </div>
+                        <div class="card">
                             <img src="img/info-diretoria/A semana iniciou com bastante trabalho_ o SINTRACEMA (3).JPG" class="card-img-top" loading="lazy" alt="Noticia:  A semana iniciou com bastante trabalho, o SINTRACEMA-Sindicato dos Agentes de Combate às Endemias e Sindacs- Sindicato dos Agentes Comunitários de Saúde, esteve em uma reunião nesta manhã (11.04), na Prefeitura Municipal de São Luís, com Secretário de Governo o Sr. Emílio Carlos Murad e o Vereador Pavão Filho, na oportunidade protocolamos e entregamos em mãos a minuta do Projeto de Lei que versa a criação do Plano de Cargos e Carreiras dos ACE e ACS.
                             Ainda estivemos protocolando dois ofícios, um solicitando uma reunião com urgência com a Superintendente da Vigilância Sanitária, e o outro com o Secretário Municipal de Saúde de São Luís.">
                             <div class="card-body">
@@ -294,15 +198,17 @@
                               <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, dolorem!</p>
                               <a href="https://www.instagram.com/p/Cq3EmknOFTk/" class="btn btn-primary">Ver noticia</a>
                         </div>
-                    </th>
-                </tr>
-            </table>
+                        </div>
+                  <br>
             </div>
-    </footer>
-    <footer class="rodape">
+
+
+      </div>
+    <div class="rodape">
       SINDICATO - SINTRACEMA 
       <br>
       Equipe de desenvolvimento da Estácio de Sá | Laboratório de Transformação Digital
-    </footer>
-</body>
+      </div>
+      </footer>
+    </body>
 </html>
